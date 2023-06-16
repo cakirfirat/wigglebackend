17d89f0a6ce4bec40348cf8b17d21e5b3f5c18d0
cakirfirat <firatcakir47@gmail.com>
2023-06-16 21:29:53 +0300

    Test clcd

--- cmd/main.go
+++ cmd/main.go
@@ -22,6 +22,7 @@ func main() {
 	r.Handle("/api/v1/register", ValidateJwt(RegisterHandler)).Methods("POST")
 	r.HandleFunc("/api/v1/login", LoginHandler).Methods("POST")
 	r.HandleFunc("/api/v1/forgot-password", ForgotPasswordHandler).Methods("POST")
+	//new deneme portainer
 
 	/* PARTNER ROUTES */
 	r.Handle("/api/v1/add-partner", ValidateJwt(AddPartnerHandler)).Methods("POST")
